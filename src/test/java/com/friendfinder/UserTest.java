5043065a2da9faf19e243d43dc1762525d9b7533
Iryna Lutsyk <s244024@student.dtu.dk>
2025-11-05 22:06:39 +0100

    User test

---
+++ src/test/java/com/friendfinder/UserTest.java
@@ -0,0 +1,54 @@
+package com.friendfinder;
+
+import com.friendfinder.exceptions.InvalidEmailException;
+import com.friendfinder.exceptions.InvalidNameException;
+import com.friendfinder.exceptions.InvalidPasswordException;
+import com.friendfinder.model.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    void validEmail_shouldNotThrowException() {
+        assertDoesNotThrow(() -> User.isEmailValid("s123456@student.dtu.dk"));
+    }
+    @Test
+    void invalidEmail_shouldThrowException() {
+        assertThrows(InvalidEmailException.class, () -> User.isEmailValid("notAValidEmail"));
+    }
+    @Test
+    void shortPassword_shouldThrowException() {
+        assertThrows(InvalidPasswordException.class, () -> User.isPasswordValid("short"));
+    }
+    @Test
+    void validPassword_shouldNotThrowException() {
+        assertDoesNotThrow(() -> User.isPasswordValid("strongPass123"));
+    }
+    @Test
+    void emptyName_shouldThrowException() {
+        assertThrows(InvalidNameException.class, () -> User.isNameValid(""));
+    }
+    @Test
+    void validName_shouldNotThrowException() {
+        assertDoesNotThrow(() -> User.isNameValid("Bob"));
+    }
+    @Test
+    void nameWithNumbers_shouldThrowException() {
+        assertThrows(InvalidNameException.class, () -> User.isNameValid("Bob123"));
+    }
+    @Test
+    void testGettersAndSetters() {
+        User user = new User();
+        user.setId(1);
+        user.setName("Bob");
+        user.setEmail("s123456@student.dtu.dk");
+        user.setPassword("password123");
+
+        assertEquals(1, user.getId());
+        assertEquals("Bob", user.getName());
+        assertEquals("s123456@student.dtu.dk", user.getEmail());
+        assertEquals("password123", user.getPassword());
+    }
+
+}
